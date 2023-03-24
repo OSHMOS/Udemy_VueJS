902e9d7fa5d9e77d64b8e42cb51822519b84a017
oshmos <ojames9707@gmail.com>
2023-03-24 17:12:17 +0900

    VueJS Quickstart: 03.24

---
+++ rebuild_the_app_with_vue/app.js
@@ -0,0 +1,14 @@
+Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      enteredValue: ''
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+      this.enteredValue = '';
+    }
+  }
+}).mount('#app'); // 어디서 적용해야하는지 vue에게 알려주는 중
